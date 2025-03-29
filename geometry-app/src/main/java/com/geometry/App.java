3d41e0a8902f279f0306ee9141991ca878004627
AntonP17 <pokrovskii.anton@mail.ru>
2025-03-29 20:45:48 +0300

    base clases geometry shapes

--- geometry-app/src/main/java/com/geometry/App.java
+++ geometry-app/src/main/java/com/geometry/App.java
@@ -6,8 +6,17 @@ package com.geometry;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(3.0, 4.0);
+        Triangle triangle = new Triangle(3.0, 4.0, 5.0);
+
+        System.out.println("circle : " + circle.getRadius());
+        System.out.println("rectangle : " + rectangle.getWidth() + ", " + rectangle.getHeight());
+        System.out.println("triangle : " + triangle.getSideA() + ", " + triangle.getSideB() + ", " + triangle.getSideC());
+
+
     }
 }
---
+++ geometry-lib/src/main/java/com/geometry/Circle.java
@@ -0,0 +1,14 @@
+package com.geometry;
+
+public class Circle {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
